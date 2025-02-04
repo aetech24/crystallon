3e7ef44a39104d032ad139fe085137b12ead7161
Stedan1234 <aetech.hub@aetechorg.com>
2025-02-04 16:58:36 +0000

    refactor: update image import paths and add Next.js image configuration

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    disableStaticImages: false,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      type: 'asset/resource'
+    });
+    return config;
+  }
+}
+
+module.exports = nextConfig
